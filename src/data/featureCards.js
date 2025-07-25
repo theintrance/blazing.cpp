eb86f5d3a01d6174cf585d4e83f0d100db2bedca
Jayden <cjjunk29@gmail.com>
2025-07-25 10:58:25 +0900

    Refactor feature card layout and update data sources
    - Adjusted margin styles in `FeatureCard.astro` for better spacing.
    - Replaced outdated feature card entries in `featureCards.js` with new links and authors.
    - Simplified the rendering logic in `index.astro` by removing unnecessary filtering and sorting controls, enhancing the user interface.

--- src/data/featureCards.js
+++ src/data/featureCards.js
@@ -5,44 +5,19 @@ export const featureCards = [
     author: "BayernMuller"
   },
   {
-    url: "https://github.com/withastro/astro",
-    date: "2024-01-15",
-    author: "astro"
+    url: "https://jin-uu.github.io/blog/250719-fast-cosine-similarity/",
+    date: "2025-07-19",
+    author: "jin-uu"
   },
   {
-    url: "https://github.com/tailwindlabs/tailwindcss",
-    date: "2024-01-10",
-    author: "tailwindlabs"
+    url: "https://bayernmuller.github.io/blog/250712-string-and-simd/",
+    date: "2025-07-12",
+    author: "BayernMuller"
   },
   {
-    url: "https://github.com/vuejs/vue",
-    date: "2024-01-08",
-    author: "vuejs"
-  },
-  {
-    url: "https://github.com/facebook/react",
-    date: "2024-01-05",
-    author: "facebook"
-  },
-  {
-    url: "https://github.com/nodejs/node",
-    date: "2024-01-03",
-    author: "nodejs"
-  },
-  {
-    url: "https://astro.build/blog/astro-4/",
-    date: "2023-12-05",
-    author: "astro"
-  },
-  {
-    url: "https://tailwindcss.com/blog/tailwindcss-v3-4",
-    date: "2023-12-10",
-    author: "tailwindlabs"
-  },
-  {
-    url: "https://reactjs.org/blog/2023/03/16/introducing-react-dev.html",
-    date: "2023-03-16",
-    author: "facebook"
+    url: "https://bayernmuller.github.io/blog/250705-simd-instruction/",
+    date: "2025-07-05",
+    author: "BayernMuller"
   }
 ];
 
